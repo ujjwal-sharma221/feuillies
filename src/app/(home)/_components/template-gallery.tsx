d46144b51caf6e1275ec2fe7b4c6833298c1c4a9
Ujjwal Sharma <ujjwalshrm2@gmail.com>
2024-12-08 01:44:18 +0530

    carousel

--- src/app/(home)/_components/template-gallery.tsx
+++ src/app/(home)/_components/template-gallery.tsx
@@ -1,16 +1,54 @@
 "use client";
 
-import { Carousel, CarouselContent } from "@/components/ui/carousel";
-
-const TEMPLATES = [{ id: "blank", label: "Blank" }];
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { TEMPLATES } from "@/constants/templates";
+import { cn } from "@/lib/utils";
 
 export function TemplateGallery() {
+  const isCreating = false;
+
   return (
     <div className="">
       <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
         <h3 className="font-medium">Start a New Document</h3>
         <Carousel>
-          <CarouselContent className="-ml-4"></CarouselContent>
+          <CarouselContent className="-ml-4">
+            {TEMPLATES.map((template) => (
+              <CarouselItem
+                key={template.id}
+                className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 2xl:basis-[14.244%] pl-4"
+              >
+                <div
+                  className={cn(
+                    "aspect-[3/4] flex flex-col gap-y-2.5",
+                    isCreating && "pointer-events-none opacity-50",
+                  )}
+                >
+                  <button
+                    disabled={isCreating}
+                    className="size-full hover:border-purple-500 rounded-sm border transition flex flex-col items-center justify-center gap-y-4"
+                    style={{
+                      backgroundImage: `url(${template.imageUrl})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      backgroundRepeat: "no-repeat",
+                    }}
+                  ></button>
+                  <p className="text-sm font-medium truncate">
+                    {template.label}
+                  </p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
         </Carousel>
       </div>
     </div>
---
+++ src/constants/templates.ts
@@ -0,0 +1,33 @@
+export const TEMPLATES = [
+  { id: "blank", label: "Blank", imageUrl: "" },
+  {
+    id: "software",
+    label: "Software-Development Proposal",
+    imageUrl: "/documents/software-proposal.svg",
+  },
+  {
+    id: "project",
+    label: "Project Proposal",
+    imageUrl: "/documents/project-proposal.svg",
+  },
+  {
+    id: "cover",
+    label: "Cover Letter",
+    imageUrl: "/documents/cover-letter.svg",
+  },
+  {
+    id: "letter",
+    label: " Letter",
+    imageUrl: "/documents/letter.svg",
+  },
+  {
+    id: "business",
+    label: " Business Letter",
+    imageUrl: "/documents/business-letter.svg",
+  },
+  {
+    id: "resume",
+    label: "Resume",
+    imageUrl: "/documents/resume.svg",
+  },
+];
